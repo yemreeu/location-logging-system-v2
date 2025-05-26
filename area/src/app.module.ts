024e8b417a2af8ff9123d591220f231696c5968d
Yunus <y.emreuyanik@gmail.com>
2025-05-27 01:22:50 +0300

    refactor: restructure application to use microservices architecture with Kafka

--- area/src/app.module.ts
+++ area/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import config from './config';
+import { ConfigModule } from '@nestjs/config';
+import { AreaService } from './services/area.service';
+import { CheckAreaAvailibityController } from './usecases/fetch-available-areas/check-area-availability.controller';
+import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Area } from './entities';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    DatabaseModule,
+    TypeOrmModule.forFeature([Area]),
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+  ],
+  controllers: [CheckAreaAvailibityController],
+  providers: [
+    {
+      provide: 'area-service',
+      useClass: AreaService,
+    },
+  ],
 })
 export class AppModule {}
